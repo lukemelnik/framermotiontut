767d8620d55c8ae75c6dae4b72f2879ac10ffe15
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-04-20 08:20:07 -0400

    add independent durations for tap and hover

--- src/components/gestures.tsx
+++ src/components/gestures.tsx
@@ -3,14 +3,16 @@ import { MotionConfig, motion } from "framer-motion";
 export default function Gestures() {
   return (
     <div>
-      <MotionConfig transition={{ duration: 0.8 }}>
+      <MotionConfig>
+        {/* this button has independent durations for hover and tap */}
         <motion.button
           className="bg-blue-600 mr-10 mb-5 shadow-lg text-slate-300 px-5 py-3 rounded-xl"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9, rotate: 10 }}
+          whileHover={{ scale: 1.1, transition: { duration: 0.8 } }}
+          whileTap={{ scale: 0.9, rotate: 10, transition: { duration: 0.2 } }}
         >
           Click Me
         </motion.button>
+        {/* this button has the default transition duration */}
         <motion.button
           className="bg-red-600 mb-5 shadow-lg text-slate-300 px-5 py-3 rounded-xl"
           whileHover={{ scale: 1.1 }}

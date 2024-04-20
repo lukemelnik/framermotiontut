6564e7a6675362e038dab1c2154ac58585018915
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-04-20 08:16:58 -0400

    add motion config for multiple buttons

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 1. Basic motion with initial, animate & transition properties. If you wrap your motion.div element in `<AnimatePresence>` then you can add an 'exit' property that it will go to before being removed from view.
 
 2. Add keyframes with an array. For transitions the times are percentages between 0/1 with the total time set by duration
+
+3. Gestures: whileHover & whileTap, can still controll durations with transition.
+
+4. Use `<MotionConfig>` to apply settings to multiple elements
--- src/components/gestures.tsx
+++ src/components/gestures.tsx
@@ -1,15 +1,24 @@
-import { motion } from "framer-motion";
+import { MotionConfig, motion } from "framer-motion";
 
 export default function Gestures() {
   return (
     <div>
-      <motion.button
-        className="bg-blue-600 mb-5 shadow-lg text-slate-300 px-5 py-3 rounded-xl"
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9, rotate: 3 }}
-      >
-        Click Me
-      </motion.button>
+      <MotionConfig transition={{ duration: 0.8 }}>
+        <motion.button
+          className="bg-blue-600 mr-10 mb-5 shadow-lg text-slate-300 px-5 py-3 rounded-xl"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9, rotate: 10 }}
+        >
+          Click Me
+        </motion.button>
+        <motion.button
+          className="bg-red-600 mb-5 shadow-lg text-slate-300 px-5 py-3 rounded-xl"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9, rotate: -10 }}
+        >
+          Click Me
+        </motion.button>
+      </MotionConfig>
     </div>
   );
 }

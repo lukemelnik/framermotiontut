dcbfe17cf53776fe2b1394c45aea825349112e81
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-04-20 07:48:30 -0400

    add hide/show button

--- src/components/basics-of-motion.tsx
+++ src/components/basics-of-motion.tsx
@@ -1,5 +1,25 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 export default function BasicsOfMotion() {
-  return <div className="bg-black w-64 h-64 rounded-xl"></div>;
+  const [isVisible, setIsVisible] = useState(true);
+  return (
+    <div>
+      <button
+        className="bg-blue-600 mb-5 text-slate-300 px-5 py-3 rounded-xl"
+        onClick={() => setIsVisible(!isVisible)}
+      >
+        Hide/Show
+      </button>
+      {isVisible && (
+        <motion.div
+          className="bg-gradient-to-r from-slate-600 to-slate-950 w-64 h-64 rounded-xl"
+          initial={{ rotate: "0deg" }}
+          animate={{ rotate: "180deg" }}
+          exit={{ rotate: "0deg" }}
+          transition={{ duration: 2, ease: "backInOut" }}
+        ></motion.div>
+      )}
+    </div>
+  );
 }

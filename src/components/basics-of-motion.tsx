2056cc982d10f6ba03c8ef43587054e651df956a
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-04-20 08:00:23 -0400

    add layout animation to button

--- src/components/basics-of-motion.tsx
+++ src/components/basics-of-motion.tsx
@@ -5,25 +5,26 @@ export default function BasicsOfMotion() {
   const [isVisible, setIsVisible] = useState(true);
   return (
     <div>
-      <button
+      <motion.button
         className="bg-blue-600 mb-5 shadow-lg text-slate-300 px-5 py-3 rounded-xl"
         onClick={() => setIsVisible(!isVisible)}
+        layout
+        transition={{ delay: 1 }}
       >
         Hide/Show
-      </button>
-      <div className="w-64 h-64">
-        <AnimatePresence>
-          {isVisible && (
-            <motion.div
-              className="bg-gradient-to-r from-slate-600 to-slate-950 w-64 h-64 rounded-xl"
-              initial={{ rotate: "0deg", scale: 0 }}
-              animate={{ rotate: "180deg", scale: 1 }}
-              exit={{ rotate: "0deg", scale: 0 }}
-              transition={{ duration: 2, ease: "backInOut" }}
-            ></motion.div>
-          )}
-        </AnimatePresence>
-      </div>
+      </motion.button>
+
+      <AnimatePresence mode="popLayout">
+        {isVisible && (
+          <motion.div
+            className="bg-gradient-to-r from-slate-600 to-slate-950 w-64 h-64 rounded-xl"
+            initial={{ rotate: "0deg", scale: 0 }}
+            animate={{ rotate: "180deg", scale: 1 }}
+            exit={{ rotate: "0deg", scale: 0 }}
+            transition={{ duration: 2, ease: "backInOut" }}
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }

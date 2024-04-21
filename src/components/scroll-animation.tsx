fed8cef80e50509dcba7f691300430c671271751
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-04-21 07:50:04 -0400

    add spring animation to scroll bar

--- src/components/scroll-animation.tsx
+++ src/components/scroll-animation.tsx
@@ -1,12 +1,14 @@
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 import React from "react";
 
 export default function ScrollAnimation() {
   const { scrollYProgress } = useScroll();
+
+  const scaleX = useSpring(scrollYProgress, { stiffness: 400, damping: 90 });
   return (
     <>
       <motion.div
-        style={{ scaleX: scrollYProgress, transformOrigin: "left" }}
+        style={{ scaleX, transformOrigin: "left" }}
         className="fixed bottom-0 left-0 w-full h-10 bg-blue-900"
       ></motion.div>
       <div className="max-w-2xl space-y-10 my-32">
@@ -15,7 +17,7 @@ export default function ScrollAnimation() {
           quod? Repudiandae repellat nobis aliquam officiis. Quos, impedit cum
           excepturi earum reiciendis, sed voluptatem assumenda perferendis
           nulla, facere minima error deleniti itaque officiis repellendus
-          pariatur numquam nesciunt beatae eum. Sint, sed tempore facere modi
+          pariatur numquam nesciunt beaatae eum. Sint, sed tempore facere modi
           quibusdam sapiente porro. Qui saepe, quod velit suscipit nesciunt
           iusto voluptas! Minus labore pariatur tempora, explicabo consectetur
           nobis aperiam accusamus similique ut fugiat eveniet eaque dicta earum
